8ff1f0fbdd62b1d159d467f2651533ea3bb79d68
AlSzmigiel <aleksandraszmigiel58@gmail.com>
2021-06-22 20:39:24 +0200

    dodane jedno activity dla ekranu add_your_recipe

---
+++ RecipeApp/app/src/main/java/com/example/recipeapp/Add_your_recipe.kt
@@ -0,0 +1,13 @@
+package com.example.recipeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
+class Add_your_recipe : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_your_recipe)
+
+    }
+}
--- RecipeApp/app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ RecipeApp/app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.SSTheme)
-        setContentView(R.layout.add_your_recipe)
+        setContentView(R.layout.activity_main)
         val toolbar = findViewById(R.id.myToolbar) as Toolbar?
         setSupportActionBar(toolbar)
 
