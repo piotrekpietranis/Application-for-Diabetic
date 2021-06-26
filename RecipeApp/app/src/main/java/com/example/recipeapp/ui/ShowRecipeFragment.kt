8ed142fc9cc66753d05a8c394f2827663c4b632d
karolina14135 <karolina.wojcik1@interia.pl>
2021-06-26 19:11:06 +0200

    nawigacja do opisu przepisu

--- RecipeApp/app/src/main/java/com/example/recipeapp/ui/ShowRecipeFragment.kt
+++ RecipeApp/app/src/main/java/com/example/recipeapp/ui/ShowRecipeFragment.kt
@@ -3,12 +3,14 @@ package com.example.recipeapp.ui
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.ImageButton
 import androidx.navigation.findNavController
 import com.example.recipeapp.R
+
 
 class ShowRecipeFragment : Fragment() {
 
@@ -19,6 +21,18 @@ class ShowRecipeFragment : Fragment() {
 
         val view = inflater.inflate(R.layout.fragment_show_recipe, container, false)
 
+        val buttonShowDetails = view.findViewById<ImageButton>(R.id.show_recipe)
+
+        buttonShowDetails.setOnClickListener {
+            val action =
+                ShowRecipeFragmentDirections.actionShowRecipeFragmentToRecipeDetailsFragment()
+            view.findNavController().navigate(action)
+        }
+
+
+
+
         return view
     }
+
 }
